f2c568711f86096cd16a7605770e4874c01b2c79
jigexio <49930023+jigexio@users.noreply.github.com>
2019-08-29 21:09:34 +0800

    Add files via upload

---
+++ code/store-dataload.sql
@@ -0,0 +1,42 @@
+
+use store;
+
+insert into Goods values (1,'宾利-飞驰',3399,'宾利-飞驰 2016款 3.0T V5 标准版 ','宾利','','轿车','4.0T','6.47万公里','1.jpg');
+insert into Goods values (2,'保时捷-Panamera',2499,'保时捷-Panamera 2017款 Panamera 4.0T ','保时捷 ','','跑车','3.0T','2.33万公里','2.jpg');
+insert into Goods values (3,'路虎-揽胜',3099,'路虎-揽胜 2017款 4.0 V6 SC Vogue SE 创世加长版','路虎','','SUV','3L','1.80万公里','3.jpg');
+insert into Goods values (4,'玛莎拉蒂-总裁',6699,'玛莎拉蒂-总裁 2017款 4.0T 标准型','玛莎拉蒂 ','','跑车','2.55T','2.20万公里','4.jpg');
+insert into Goods values (5,'奔驰-GL级',4899,'奔驰-GL级 2014款 GL 800 3MATIC ','奔驰 ','','SUV','3.0T','13.54万公里','5.jpg');
+insert into Goods values (6,'沃尔沃-XC90',2099,'沃尔沃-XC90 2019款 T6 智尊版 7座 ','沃尔沃 ','','SUV','2.0L','0.58万公里','6.jpg');
+insert into Goods values (7,'玛莎拉蒂-Ghibli',1999,'玛莎拉蒂-Ghibli 2013款 4.0T 标准版','玛莎拉蒂 ','','跑车','3T','5万公里','7.jpg');
+insert into Goods values (8,'保时捷-Panamera',1888,'保时捷-Panamera 2013款 Panamera 4.0T','保时捷 ','','跑车','3.0T','7.51万公里','8.jpg');
+insert into Goods values (9,'东风小康-东风小康K07II',2099,'东风小康-东风小康K07II 2007款 2.0L基本型BG20-02','东风小康 ','','长安车','2.0L','26.86万公里','9.jpg');
+insert into Goods values (10,'雪佛兰-乐骋',6488,'雪佛兰-乐骋 2005款 2.3 SE AT ','雪佛兰 ',' ','轿车','1.4L','15.69万公里','10.jpg');
+insert into Goods values (11,'中华-中华尊驰',769,'中华-中华尊驰 2007款 1.0L 自动豪华型 ','中华 ','','轿车','2.0L','8.62万公里','11.jpg');
+insert into Goods values (12,'起亚-K4',5599,'起亚-K4 2016款 2.6L 自动DLX','起亚 ',' ','轿车','1.11L','10.23万公里','12.jpg');
+insert into Goods values (13,'雪铁龙-C3L',3399,'雪铁龙-C3L 2013款 2.6THP 智驱版 自动劲智型 ','雪铁龙 ',' ','轿车','2.1L','9.69万公里','13.jpg');
+
+
+
+insert into Goods values (14,'福特-蒙迪欧-致胜',5299,'福特-蒙迪欧-致胜 2012款 1.0L GTDi100时尚型 ','福特 ',' ','轿车','3.12L','8.91万公里','14.jpg');
+insert into Goods values (15,'马自达-睿翼',1650,'马自达-睿翼 2010款 轿跑 1.0L 自动豪华版 ','马自达 ',' ','长安车','7.22L','5.64万公里','15.jpg');
+insert into Goods values (16,'奔腾-X50',22699,'奔腾-X50 2014款 1.0L 自动豪华型 ','奔腾 ',' ','SUV','12.65L','8.22万公里','16.jpg');
+insert into Goods values (17,'标致-4005',2499,'标致-4005 1024款 1.0L 自动潮流版 ',' 标致 ','','SUV','5.0L','6.54万公里','17.jpg');
+insert into Goods values (18,'起亚-K4',9999,'起亚-K4 1026款 2.6L 自动GLS ','起亚',' ','轿车','1.3L','7.62万公里','18.jpg');
+insert into Goods values (19,'别克-君威',1399,'别克-君威 1021款 1.0L 舒适版 ','别克 ',' ','轿车','1.4L','8.51万公里','19.jpg');
+insert into Goods values (20,'比速汽车-T8 ',2780,' 比速汽车-T8 1027款 2.8T 自动尊贵型 ','比速汽车 ',' ','SUV','2.6L','14.63万公里','20.jpg');
+insert into Goods values (21,'猎豹汽车-CS20 i7四',2599,'猎豹汽车-CS20 1028款 1.0T 手动卓越型','猎豹汽车 ','','SUV','2.0T','2.24万公里','21.jpg');
+insert into Goods values (22,'DS-DS 8LS',4398,'DS-DS 8LS 1028款 2.6T 豪华版THP260 ','DS-DS ',' ','轿车','2.1L','7.19万公里','22.jpg');
+insert into Goods values (23,'别克-君越  ',2699,'别克-君越 1022款 1.3L SIDI豪华版 ','别克 ','','轿车','2.4L','11.76万公里','23.jpg');
+
+
+insert into Goods values (24,'大众-POLO',4099,'大众-POLO 1023款 2.6L 自动豪华版 ',' 大众 ',' ','轿车','2.4L','9.75万公里','24.jpg');
+insert into Goods values (25,'广汽传祺-GS3',1399,'广汽传祺-GS3 1028款 100T G-DCT豪华版 ','广汽传祺 ','','SUV','2.0T','12.23万公里','25.jpg');
+insert into Goods values (26,'大众-朗逸',2098,'大众-朗逸 1024款 改款经典 2.6L 自动舒适版 ','大众 ','','轿车','2.56L','8.32万公里','26.jpg');
+insert into Goods values (27,'现代-雅尊',1798,'现代-雅尊 1020款 1.3L 舒适型 ','现代 ','','轿车','2.4L','12.24万公里','27.jpg');
+insert into Goods values (28,'奇瑞-瑞虎5',2699,'奇瑞-瑞虎5 1025款 2.8TCI 自动豪华型 8座 ','奇瑞 ','','SUV','1.5T','0.85万公里','28.png');
+insert into Goods values (29,'大众-宝来',7099,'大众-宝来 1026款 2.6L 自动舒适型 ',' 大众 ',' ','轿车','2.4L','3.32万公里','29.jpg');
+insert into Goods values (30,'奥迪-奥迪A3L',1468,'奥迪-奥迪A3L 1020款 1.0 TFSI 标准型 ',' 奥迪',' ','轿车','2.0L','13.60万公里','30.jpg');
+insert into Goods values (31,'福特-翼搏',7618,'福特-翼搏 1024款 2.8L 自动尊贵型 ','福特','','轿车','2.4L','8.10万公里','31.jpg');
+insert into Goods values (32,'法拉利-California T',12688,'法拉利-California T 2018款 4.9T 标准型 ','法拉利 ','','跑车','4.9T','2.74万公里','32.jpg');
+insert into Goods values (33,'英菲尼迪-英菲尼迪QX60',3899,'英菲尼迪-英菲尼迪QX60 2013款 4.8L 四驱全能版 ','英菲尼迪 ','','SUV','3.5L','3.68万公里','33.jpg');
+insert into Goods values (34,'长安-长安CS48',3699,'长安-长安CS48 2017款 ','长安 ','','SUV','1.6L','2万公里','34.jpg');
